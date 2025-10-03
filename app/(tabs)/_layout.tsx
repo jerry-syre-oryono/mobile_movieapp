8ccc18ee2281a22aa8def12e99f5c8d95f87831f
jerry-syre-oryono <jeremyoryono@gmail.com>
2025-10-03 10:11:37 +0300

    feat(tabs): add search, profile and saved screens to tab navigation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -10,6 +10,28 @@ const _layout = () => {
             title: "Home",
         }}
         />
+        <Tabs.Screen
+        name="search"
+        options={{
+            headerShown: false,
+            title: "Search",
+        }}
+        />
+        <Tabs.Screen
+        name="profile"
+        options={{
+            headerShown: false,
+            title: "Profile",
+        }}
+        />
+        <Tabs.Screen
+        name="saved"
+        options={{
+            headerShown: false,
+            title: "Saved",
+        }}
+        />
+        
     </Tabs>
   )
 }
