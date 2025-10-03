01b9cce9859473e86fcee721affa8031140bb56d
jerry-syre-oryono <jeremyoryono@gmail.com>
2025-10-03 11:48:41 +0300

    feat(assets): add new icons and images while removing unused ones
    - Add new set of icons (home, search, person, etc.) and images (bg, highlight, etc.)
    - Remove old unused image assets (react-logo, favicon, etc.)
    - Update app.json to use new logo as icon
    - Add type definitions for images and interfaces
    - Implement tab bar with new icons and styling

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,4 +1,11 @@
 import { Tabs } from 'expo-router'
+import { ImageBackground } from 'react-native'
+import {images} from '@/constants/images'
+import { icons } from '@/constants/icons'
+import { Image, Text } from 'react-native'
+
+
+
 
 const _layout = () => {
   return (
@@ -8,29 +15,51 @@ const _layout = () => {
         options={{
             headerShown: false,
             title: "Home",
-        }}
+            tabBarIcon:({focused}) => (
+                <ImageBackground 
+                    source={images.highlight}
+                    className='flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden'
+                >
+                   <Image source={icons.home} tintColor='#151312' className='size-5'/>
+                   <Text className="text-secondary text-base font-semibold ml-2">Home</Text>
+                </ImageBackground>
+            )
+         }}
         />
+        
         <Tabs.Screen
         name="search"
         options={{
             headerShown: false,
             title: "Search",
+            tabBarIcon:({focused}) => (
+                <Image source={icons.search} className='size-5'/>
+            )
         }}
         />
-        <Tabs.Screen
-        name="profile"
-        options={{
-            headerShown: false,
-            title: "Profile",
-        }}
-        />
+
         <Tabs.Screen
         name="saved"
         options={{
             headerShown: false,
             title: "Saved",
+            tabBarIcon:({focused}) => (
+                <Image source={icons.save} className='size-5'/>
+            )
         }}
         />
+
+        <Tabs.Screen
+        name="profile"
+        options={{
+            headerShown: false,
+            title: "Profile",
+            tabBarIcon:({focused}) => (
+                <Image source={icons.person} className='size-5'/>
+            )
+        }}
+        />
+        
         
     </Tabs>
   )
